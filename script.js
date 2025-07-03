e8f917f09e1b5a9bf8f3626fc0a502e170c5d504
Asikur Rahman <mdasikur5893@gmail.com>
2025-07-03 23:13:24 +0600

    increase commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var count = 0;
+
+function increase(){
+   count ++;
+   document.getElementById('count').innerHTML = count;
+}
+
+
+function decrease(){
+    count --;
+    document.getElementById('count').innerHTML = count;
+}
